b58ade2c1b6004b622deda31b8bf18445b593b7c
Nazia Imran <161065738+NaziaImran786@users.noreply.github.com>
2024-10-18 11:53:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 // alert("loading")
-//----------------------------------------------- Adding new field in work experience
+//------------------------------------------function Adding new field in work experience-------------------------
 function addNewWEField() {
     let newNode = document.createElement("textarea");
     newNode.classList.add("form-control");
@@ -12,7 +12,7 @@ function addNewWEField() {
     let weAddButtonOb = document.getElementById("weAddButton");
     weOb.insertBefore(newNode, weAddButtonOb);
 }
-// ---------------------------------------------Adding new field in academic Qualification
+// -------------------------------------function Adding new field in academic Qualification------------------------
 function addNewAQField() {
     let newNode = document.createElement("textarea");
     newNode.classList.add("form-control");
@@ -24,74 +24,85 @@ function addNewAQField() {
     let aqAddButtonOb = document.getElementById("aqAddButton");
     aqOb.insertBefore(newNode, aqAddButtonOb);
 }
-//-------------------------------------------------------------generate resume
+//---------------------------------------------------function generate resume------------------------------------
 function generateResume() {
+    //---------------------------------------------------------in variable name get --------------------------------
     let name = document.getElementById("nameField").value;
     let nameT1 = document.getElementById("nameT1");
     nameT1.innerHTML = name;
-    //--------------------------------------------------------------------------direct
-    //---------------------------------------------------------------------------name
+    //--------------------------------------------------------------------------direct name get----------------------
     document.getElementById("nameT2").innerHTML = name;
-    //-------------------------------------------------------------------------contact
+    //-------------------------------------------------------------------------contact-------------------------------
     document.getElementById("contactT").innerHTML =
         document.getElementById("contactField").value;
-    //------------------------------------------------------------------------address
+    //------------------------------------------------------------------------address--------------------------------
     document.getElementById("addressT").innerHTML =
         document.getElementById("addressField").value;
-    //----------------------------------------------------------------------- linkedIn-
+    //----------------------------------------------------------------------- linkedIn------------------------------
     document.getElementById("linkT").innerHTML =
         document.getElementById("linkedinField").value;
-    //-----------------------------------------------------------------------facebook
+    //-----------------------------------------------------------------------facebook-------------------------------
     document.getElementById("fbT").innerHTML =
         document.getElementById("fbField").value;
-    //------------------------------------------------------------------------objective
+    // /------------------------------------------------------------------------github------------------------------
+    document.getElementById("githubT").innerHTML =
+        document.getElementById("githubField").value;
+    //------------------------------------------------------------------------objective------------------------------
     document.getElementById("objectiveT").innerHTML =
         document.getElementById("objectiveField").value;
-    //-------------------------------------------------------------------work experience
+    //-------------------------------------------------------------------work experience----------------------------
     let we = document.getElementsByClassName("weField");
     let str = "";
     for (let e of we) {
-        str = str + `<li> ${e.value} </li>`;
+        str = str + `<li>${e.value}</li>`;
     }
     document.getElementById("weT").innerHTML = str;
-    //-------------------------------------------------------------academic Qualification
+    //-------------------------------------------------------------academic Qualification------------------------------
     let aqs = document.getElementsByClassName("eqField");
     let str1 = "";
     for (let e of aqs) {
-        str1 = str1 + `<li> ${e.value} </li>`;
+        str1 = str1 + `<li>${e.value}</li>`;
     }
     document.getElementById("aqT").innerHTML = str1;
-    //-----------------------------------------------------------------code for setting image
+    //-----------------------------------------------------------------code for setting image--------------------------
     let file = document.getElementById("imgField").files[0];
     // console.log(file);
     let reader = new FileReader();
     reader.readAsDataURL(file);
     // console.log(reader.result);
-    //----------------------------------------------------------------set the image to template
+    //----------------------------------------------------------------set the image to template------------------------
     reader.onloadend = function () {
         document.getElementById("imgTemplate").src = reader.result;
     };
     document.getElementById("cv-form").style.display = "none";
     document.getElementById("cv-template").style.display = "block";
     const username = name ? name.toLowerCase().replace(/\s+/g, '-') : 'user'; //Generate username from the name
-    const baseUrl = 'https://hackathon-milestone-5-ruddy.vercel.app/'; // Your Vercel deployment URL
-    const uniqueResumeUrl = `${baseUrl}?/${username}`; // Create unique URL
-    //-----------------------------------------------------------Set the resume link in the DOM
+    const baseUrl = 'http://127.0.0.1:5501/index.html'; // Your Vercel deployment URL
+    const uniqueResumeUrl = `${baseUrl}?/${username}`; // Create unique URL      
+    //-----------------------------------------------------------Set the resume link in the DOM--------------------------
     const resumeLink = document.getElementById('resumeLink');
     resumeLink.setAttribute('href', uniqueResumeUrl);
     resumeLink.textContent = uniqueResumeUrl;
-    //------------------------------------------------------Copy link to clipboard functionality
+    //------------------------------------------------------Copy link to clipboard functionality------------------------
     document.getElementById('copyLinkBtn').addEventListener('click', () => {
         navigator.clipboard.writeText(uniqueResumeUrl).then(() => {
             alert('Resume link copied to clipboard!');
         });
     });
 }
-//---------------------------------------------------------------- print resume
+//---------------------------------------------------------------- print resume------------------------------------
 function printResume() {
+    const form = document.getElementById("cv-form");
+    const editBtn = document.getElementById("editBtn");
+    form.style.display = "none";
+    if (editBtn)
+        editBtn.style.display = "none";
     window.print();
+    form.style.display = "block";
+    if (editBtn)
+        editBtn.style.display = "block";
 }
-// ----------------------------------------------------------------resume editing
+// ----------------------------------------------------------------resume editing------------------------------------
 function editResume() {
     let editBtn = document.getElementById("editBtn");
     editBtn.style.display = editBtn.style.display === "none" ? "block" : "none";
